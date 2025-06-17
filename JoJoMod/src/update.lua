3c670506c68b7f33830dae1b287ce315c6c3ea1d
TheCoroboCorner <81382526+TheCoroboCorner@users.noreply.github.com>
2025-06-17 16:06:55 -0400

    Balatro Mod Manager semi-support
    (DEBUG)
    - Added further multi-mod support and API support
    - Added possibility where Mod Manager ID can be used in place of repo/owner tags. Not necessary, though.
    - Added multi-version support

--- JoJoMod/src/update.lua
+++ JoJoMod/src/update.lua
@@ -4,13 +4,14 @@ local bit = require("bit")
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 local git_owner = "TheCoroboCorner" -- This is the Github username to look up for the repository
-
 local git_repo = "JoJoMod" -- This is the name of the Github repository in question
 
-local mod_path = "Mods/JoJoMod" -- This is the path to your mod folder
+local mod_index_id = nil -- This is an alternative to git_owner and git_repo, for if those either don't work or you don't want to use them.
+						 -- As such, you only really *need* to have either this or them.
 
 local id = "JoJoMod" -- This is the ID of your mod -- the same one you put in your metadata file
 
+local mod_path = "Mods/JoJoMod" -- This is the path to your mod folder
 local subpath = "JoJoMod" -- Some mods have additional folders for you to dig through once you open the zip.
 -- OPTIONAL				  -- In this case, mine is named JoJoMod because I zip the folder instead of the contents.
 						  
@@ -27,15 +28,17 @@ local target_version = nil -- This is the version of the mod that's preferred --
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+local current_mod = nil
+
 --- Fetches the body of the given url using Curl.
 --- 	The URL in question is inputted, then it outputs the corresponding JSON file for its body component.
 local function curl_fetch(url)
 	local cmd = ('curl -sL "%s"'):format(url:gsub('"','\\"'))
 	local fp = io.popen(cmd, "r")
-	if not fp then return nil, "curl not available" end
+	assert(fp, "curl not available")
 	local body = fp:read("*a")
 	local ok, _, exit = fp:close()
-	if not ok then return nil, ("curl exited with code %s"):format(tostring(exit)) end
+	assert(ok, ("curl exited with code %s"):format(tostring(exit)))
 	return body
 end
 
@@ -61,27 +64,49 @@ end
 --- Returns the tag and asset_url of the mod you specified in your metadata file with the git_owner and git_repo slots.
 ---		It constructs the URL of the Github project associated with the name and repo listed,
 --- then it feeds that through to parse_release() and returns what it has.
-function check_version()
-	local owner, repo = SMODS.Mods[id].git_owner, SMODS.Mods[id].git_repo
+function check_version(overflowProtection)	
+	if git_owner and git_repo then
+		local url = (not target_version or target_version == "-1") and string.format("https://api.github.com/repos/%s/%s/releases/latest", git_owner, git_repo)
+					or string.format("https://api.github.com/repos/%s/%s/releases/tags/%s", git_owner, git_repo, 'v' .. target_version)
+		
+		local body, err = curl_fetch(url)
+		if not body then
+			url = string.format("https://api.github.com/repos/%s/%s/releases/tags/%s", git_owner, git_repo, target_version)
+			body, err = curl_fetch(url)
+			if not body then
+				print("Fetch failed:", err)
+				return nil
+			end
+		end
+		
+		local tag, asset_url, perr = parse_release(body)
+		if not tag then
+			if body.message == "No server is currently available to service your request. Sorry about that. Please try resubmitting your request and contact us if the problem persists." then
+				print("Network connection error. Connection canceled.")
+			else
+				print("Couldn't parse JSON - tag_name missing")
+			end
+			return nil, nil, perr
+		end
+		
+		return tag, asset_url, nil
+	elseif mod_index_id then
+		if overflowProtection then
+			print("Fetch failed: invalid repo")
+			return nil, nil, nil
+		end
 	
-	if not owner then return nil, "git_owner parameter not found in metadata json file" end
-	if not repo then return nil, "git_repo parameter not found in metadata json file" end
-	
-	local url = string.format("https://api.github.com/repos/%s/%s/releases/latest", owner, repo)
-	
-	local body, err = curl_fetch(url)
-	if not body then
-		print("Fetch failed:", err)
-		return nil
+		local release_json = curl_fetch("https://api.github.com/repos/skyline69/balatro-mod-index/releases/latest")
+		local release = json.decode(release_json)
+		local tag = release.tag.name
+		
+		local meta_url = ("https://api.github.com/repos/skyline69/balatro-mod-index/contents/mods/%s/meta.json?ref=%s"):format(mod_index_id, tag)
+		local raw_json = curl_fetch(meta_url)
+		
+		git_owner, git_repo = raw_json.repo:match("^github%.com[:/]+([^/]+)/([^/]+)(?:%.git)?/?$")
+		
+		return check_version(true)
 	end
-	
-	local tag, asset_url, perr = parse_release(body)
-	if not tag then
-		print("Couldn't parse JSON - tag_name missing")
-		return nil, nil, perr
-	end
-	
-	return tag, asset_url, nil
 end
 
 --- Downloads the file in question using Curl.
@@ -113,8 +138,8 @@ local function unzip_file(zip_path)
 	mod_path = mod_path:gsub("/", "\\")
 	zip_path = zip_path:gsub("/", "\\")
 
-	local is_windows = package.config:sub(1,1) == '\\'
 	local tmp_dir = mod_path .. "_tmp"
+	local is_windows = package.config:sub(1,1) == '\\'
 	local ok1, ok2, ok3
 	
 	if is_windows then
@@ -146,7 +171,6 @@ local function unzip_file(zip_path)
 			(ok2 == true or ok2 == 0) and
 			(ok3 == true or ok3 == 0) and
 			ok4 == true
-	
 end
 
 --- Downloads the latest update for the mod in question.
@@ -155,16 +179,15 @@ end
 --- asset url, so it can install the correct update and the correct file for that update. Then, it
 --- constructs the correct URL and attempts to download it using the download_file function.
 local function download_update()
-	local owner, repo = SMODS.Mods[id].git_owner, SMODS.Mods[id].git_repo
-	if owner and repo then
+	if (git_owner and git_repo) or (mod_index_id) then
 		local tag, asset_url, err = check_version()
 		if not tag then
 			print("Version check failed:", err)
 			return false, nil
 		end
 		
-		local zip_url = asset_url or string.format("https://github.com/%s/%s/archive/%s.zip", owner, repo, tag)
-		local zip_path = ("Mods\\%s-%s.zip"):format(repo, tag)
+		local zip_url = asset_url or string.format("https://github.com/%s/%s/archive/%s.zip", git_owner, git_repo, tag)
+		local zip_path = ("Mods\\%s-%s.zip"):format(git_repo, tag)
 		
 		if not download_file(zip_url, zip_path) then
 			print("Failed to download update from " ..zip_url)
@@ -187,11 +210,32 @@ local function install_update(zip_path)
 	return true
 end
 
+--- Updates all the context variables to match the current mod.
+--- 	These are all the variables that are mentioned at the top of the file, that you find
+---	in the metadata json file. We use the local files up there mainly because we have them there
+--- anyway for demonstrative purposes and it saves us the time of array-accessing current_mod.
+local function updateArgs()
+	if not current_mod then return nil end
+	
+	git_owner = current_mod.git_owner
+	git_repo = current_mod.git_repo
+	mod_path = current_mod.mod_path
+	id = current_mod.id
+	subpath = current_mod.subpath
+	download_suffix = current_mod.download_suffix
+	update_mandatory = current_mod.update_mandatory
+	target_version = current_mod.target_version
+end
+
 --- Downloads, then installs the update.
 --- 	restart just depends on the scenario - an individual update might want restart to be enabled,
 --- but when it's in a bigger scheme, you might want to turn it off, just so you can retrigger it later.
-local function update_game(restart)
+---	mod just sets what mod to update. By default, it will do the mod in the current context (current_mod),
+--- but it can be whatever mod you want.
+local function update_mod(mod, restart)
 	restart = restart or true
+	current_mod = mod or current_mod
+	updateArgs()
 	
 	local download_result, download_path = download_update()
 	if not download_result then return false end
@@ -205,7 +249,7 @@ end
 --- Installs the update when the 'Yes' confirmation button is pressed in the update prompt screen.
 --- 	G.FUNCS.exit_overlay_menu() is there in case something goes wrong, so that it'll close anyway.
 G.FUNCS.update_accepted = function(e)
-	update_game()
+	update_mod()
 	G.FUNCS.exit_overlay_menu()
 end
 
@@ -222,9 +266,9 @@ end
 --- After the text is instantiated, it moves onto the singular text node in question, then following with the
 --- buttons, and ending with the root node (It feels really messy to have all the nested tables in one place,
 --- so I did this). Finally, it overlays the menu on the screen.
-local function show_update_prompt(latest, current, args)
+local function show_update_prompt(latest, current)
 	local msg = {
-		("A new version of %s is available!\n"):format(SMODS.Mods[id].name),
+		("A new version of %s is available!\n"):format(current_mod.name),
 		("Installed: %s\n"):format(current),
 		("Latest:    %s\n\n"):format(latest),
 		"Update now? (This will restart Balatro.)"
@@ -395,33 +439,35 @@ end
 --- a lot easier to compare that way (using V), and it even works for alpha/beta versions. Sometimes, if there are
 --- connection errors, check_version() will return nil, so there has to be a null check at the start there.
 JOJO.update_check = function(args, prompt)
+	-- Lets args be used for all the other functions needed
+	current_mod = args
 	if not args then return false, "args not found" end
-	mod_path = args.mod_path
-	id = args.id
-	subpath = args.subpath
-	download_suffix = args.download_suffix
+	
+	-- Sets all the local variables properly
+	updateArgs()
 	
 	local git_version = check_version()
-	if not git_version then return nil, "There appears to have been a connection error" end
+	if not git_version then return nil, "There appears to have been a connection error" end -- This should only happen if there's been a connection error
 	
+	-- Sets the target version to look for
 	local latest_version = git_version:match("^v?(%d+%.%d+%.%d+%a*)$")
-	target_version = (not args.target_version or args.target_version == "-1") and latest_version or args.target_version
+	target_version = (not current_mod.target_version or current_mod.target_version == "-1") and latest_version or current_mod.target_version
 	
-	local current_version = args.version:match("^v?(%d+%.%d+%.%d+%a*)$")
+	local current_version = current_mod.version:match("^v?(%d+%.%d+%.%d+%a*)$")
 	
-	if target_version and current_version and V(target_version) > V(current_version) then
+	if target_version and current_version and V(target_version) > V(current_version) then -- if the target version is newer than the current version
 		if prompt then
-			show_update_prompt('v' .. target_version, 'v' .. current_version, args)
+			show_update_prompt('v' .. target_version, 'v' .. current_version)
 		end
 		return true, "Version too old"
-	elseif target_version and current_version and V(target_version) < V(current_version) then
+	elseif target_version and current_version and V(target_version) < V(current_version) then -- if the target version is older than the current version
 		if prompt then
-			show_update_prompt('v' .. target_version, 'v' .. current_version, args)
+			show_update_prompt('v' .. target_version, 'v' .. current_version)
 		end
 		
-		if latest_version ~= target_version and V(latest_version) < V(current_version) then
+		if latest_version ~= target_version and V(latest_version) < V(current_version) then -- if the latest version is older than the current version
 			return true, "Developer version, newer than newest version"
-		elseif V(latest_version) == V(current_version) then
+		elseif V(latest_version) == V(current_version) then -- if the current version is the latest version
 			return true, "Latest version too new"
 		end
 			
